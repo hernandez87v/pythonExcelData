f63728d477ddf639635a26989fc6bbe7c343d830
hernandez87v <hernandez87v@hotmail.com>
2021-08-29 14:38:12 -0700

    add dropna() to remove NaN values

--- getExcelData.py
+++ getExcelData.py
@@ -8,5 +8,6 @@ appended_data = []
 for sheet_name in worksheets:
     produce = "PRODUCE"
     df = pd.read_excel(spreadsheet_file, sheet_name, header=0)
-    df = df[[produce, "POUNDS SOLD"]].where(df["POUNDS SOLD"] > 29.4)
+    df = df[[produce, "POUNDS SOLD"]].where(df["POUNDS SOLD"] > 39.5).dropna()
+    # df = df.dropna()
     print(df)
