4154b1716160e160d7c331b530e71a87634e2de5
hernandez87v <hernandez87v@hotmail.com>
2021-08-29 14:59:06 -0700

    add append_data output to New excel sheet

--- getExcelData.py
+++ getExcelData.py
@@ -9,5 +9,10 @@ for sheet_name in worksheets:
     produce = "PRODUCE"
     df = pd.read_excel(spreadsheet_file, sheet_name, header=0)
     df = df[[produce, "POUNDS SOLD"]].where(df["POUNDS SOLD"] > 39.5).dropna()
-    # df = df.dropna()
-    print(df)
+    df["Product ID"] = sheet_name
+    df = df[["Product ID", "POUNDS SOLD", produce]]
+    appended_data.append(df)
+
+appended_data = pd.concat(appended_data)
+appended_data.to_excel(
+    "/mnt/HDD/CODE/pythonExcelData/output.xlsx")
