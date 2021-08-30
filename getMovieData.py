125a160386745e309c8b0cb668183575c699edcb
hernandez87v <hernandez87v@hotmail.com>
2021-08-29 18:36:31 -0700

    add 2 other sheets to be read from excel sheet

--- getMovieData.py
+++ getMovieData.py
@@ -7,4 +7,12 @@ movies = pd.read_excel(excel_file)
 
 movies_sheet1 = pd.read_excel(excel_file, sheet_name=0, index_col=0)
 
-print(movies_sheet1.head())
+# print(movies_sheet1.head())
+
+movies_sheet2 = pd.read_excel(excel_file, sheet_name=1, index_col=0)
+
+# print(movies_sheet2.head())
+
+movies_sheet3 = pd.read_excel(excel_file, sheet_name=1, index_col=0)
+
+print(movies_sheet3.head())
